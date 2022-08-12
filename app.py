ec70574504546b7ab8982552d7aa03ff3e32286e
David Nugent <david.nugent2425@gmail.com>
2022-08-12 17:24:06 +0100

    use pretrained model

--- app.py
+++ app.py
@@ -1,17 +1,14 @@
 from flask import Flask, request, jsonify
-import numpy as np
-from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
+# from transformers import AutoTokenizer, TFAutoModelForSequenceClassification
 from transformers import pipeline
 
 app = Flask(__name__)
-
 
 @app.route('/', methods=['GET', 'POST'])
 def makecalc():
     if request.method=="POST":
         data = request.get_json()
         print("data ---- > ", data)
-        # prediction = np.array2string(model.predict(data))
         results = classifier(data)
         
         return jsonify(results)
@@ -20,13 +17,14 @@ def makecalc():
 
 if __name__ == '__main__':
 
-    model_path = './models/transformers/' 
-    model = TFAutoModelForSequenceClassification.from_pretrained(model_path, local_files_only=True)
-    print("----------- transformer model loaded ------------")
-    tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
-    print("----------- transformer tokenizer loaded ------------")
-    classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
-    print(classifier)
+    # model_path = './models/transformers/' 
+    # model = TFAutoModelForSequenceClassification.from_pretrained(model_path, local_files_only=True)
+    # print("----------- transformer model loaded ------------")
+    # tokenizer = AutoTokenizer.from_pretrained(model_path, local_files_only=True)
+    # print("----------- transformer tokenizer loaded ------------")
+    # classifier = pipeline('sentiment-analysis', model=model, tokenizer=tokenizer)
+    # print(classifier)
+    classifier = pipeline("fill-mask", model="camembert-base")
 
     app.run(debug=True, host='0.0.0.0')
 
